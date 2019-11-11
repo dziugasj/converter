6ba9db5d90b810788a25cda097b90c0570f77756
Džiugas Juozapaitis <dziugasj@gmail.com>
2019-11-11 23:39:15 +0200

    single unit test works

--- src/test/groovy/homework/currency/CurrencyConverterSpec.groovy
+++ src/test/groovy/homework/currency/CurrencyConverterSpec.groovy
@@ -1,4 +1,33 @@
 package homework.currency
 
-class CurrencyConverterSpec {
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+
+import java.math.MathContext
+
+import static java.math.MathContext.DECIMAL64
+
+class CurrencyConverterSpec extends Specification {
+    static final MathContext MATH_OPERATIONS_CONTEXT = DECIMAL64;
+    static final String BASE_CURRENCY_CODE = "EUR"
+    static final int SCALE = 2
+
+    Currency baseCurrency = new Currency(BASE_CURRENCY_CODE)
+
+    @Subject
+    CurrencyConverter currencyConverter = new CurrencyConverter(MATH_OPERATIONS_CONTEXT, new Currency(BASE_CURRENCY_CODE), SCALE);
+
+    @Unroll
+    def 'xxx'() {
+        given:
+            ConversionParameter parameter = new ConversionParameter(new Currency(sourceCode), new Currency(targetCode), sourceRate, targetRate, givenAmount)
+        when:
+            BigDecimal convertedAmount = currencyConverter.convert(parameter)
+        then:
+            convertedAmount == expectedAmount
+        where:
+            sourceCode | targetCode | sourceRate | targetRate | givenAmount | expectedAmount
+            'EUR'      | 'EUR'      | 1.0        | 1.0        | 200.00      | 200.00
+    }
 }
