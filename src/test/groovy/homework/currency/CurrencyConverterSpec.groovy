8de34f217d10dcd03c4d57998c72cf3c38031636
Džiugas Juozapaitis <dziugasj@gmail.com>
2019-11-12 21:39:27 +0200

    one more done

--- src/test/groovy/homework/currency/CurrencyConverterSpec.groovy
+++ src/test/groovy/homework/currency/CurrencyConverterSpec.groovy
@@ -13,13 +13,10 @@ class CurrencyConverterSpec extends Specification {
     static final String BASE_CURRENCY_CODE = "EUR"
     static final int SCALE = 2
 
-    Currency baseCurrency = new Currency(BASE_CURRENCY_CODE)
-
     @Subject
     CurrencyConverter currencyConverter = new CurrencyConverter(MATH_OPERATIONS_CONTEXT, new Currency(BASE_CURRENCY_CODE), SCALE);
 
-    @Unroll
-    def 'xxx'() {
+    def 'should return the same amount when target and source currencies are the same'() {
         given:
             ConversionParameter parameter = new ConversionParameter(new Currency(sourceCode), new Currency(targetCode), sourceRate, targetRate, givenAmount)
         when:
@@ -28,6 +25,21 @@ class CurrencyConverterSpec extends Specification {
             convertedAmount == expectedAmount
         where:
             sourceCode | targetCode | sourceRate | targetRate | givenAmount | expectedAmount
-            'EUR'      | 'EUR'      | 1.0        | 1.0        | 200.00      | 200.00
+            'EUR'      | 'EUR'      | 1.2221     | 1.0545     | 200.00      | 200.00
+    }
+
+    @Unroll
+    def 'should properly convert amount using given rates'() {
+        given:
+            ConversionParameter parameter = new ConversionParameter(new Currency(sourceCode), new Currency(targetCode), sourceRate, targetRate, givenAmount)
+        when:
+            BigDecimal convertedAmount = currencyConverter.convert(parameter)
+        then:
+            convertedAmount == expectedAmount
+        where:
+            sourceCode | targetCode | sourceRate | targetRate | givenAmount | expectedAmount
+            'EUR'      | 'PLN'      | 1.000      | 4.12120    | 200.00      | 824.24
+            'PLN'      | 'EUR'      | 4.43030    | 1.000      | 800.00      | 180.57
+            'PLN'      | 'USD'      | 4.43030    | 1.06010    | 500.00      | 119.64
     }
 }
